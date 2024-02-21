1f531cf85dbe3403c2d1c8ba1d8ebbb0e2234ed3
keyvan-malek <45242295+keyvan-malek@users.noreply.github.com>
2024-02-21 13:48:28 -0600

    License added

--- load_functions.R
+++ load_functions.R
@@ -1,3 +1,14 @@
+#############################################################################################
+
+#                    		GNU GENERAL PUBLIC LICENSE
+#                      		Version 3, 29 June 2007
+
+# 	Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+# 	Everyone is permitted to copy and distribute verbatim copies
+# 	of this license document, but changing it is not allowed.
+
+#############################################################################################
+
 ######################### Constants ##########
 
 cfsTOafw <- 1.9834 * 7 # cfs to acre-ft per week conversion
