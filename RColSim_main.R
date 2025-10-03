40f27e6267b3cc3b7ee92be6b78a89081cde3d42
keyvan-malek <45242295+keyvan-malek@users.noreply.github.com>
2025-10-02 23:01:42 -0500

    Revert "RColSim_cleanup_2"

--- RColSim_main.R
+++ RColSim_main.R
@@ -24,7 +24,6 @@
 #############################################################################################
 
 #                                      MIT License
-												  
 
 #    Copyright (c) 2025
 
@@ -45,6 +44,7 @@
 #    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #    SOFTWARE.
+
 #############################################################################################
 
 
